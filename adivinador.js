2ee546524b7739da6d1aab5ba085a3c3c91a4927
emiezeoc <emiezeoc@gmail.com>
2023-05-27 21:22:12 -0300

    Initial commit

---
+++ adivinador.js
@@ -0,0 +1,13 @@
+let numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+const $botonEnviar = document.querySelector("#enviar");
+const numeroDeIntentos = 0;
+
+
+$botonEnviar.onclick = function() {
+      alert('El botón funciona');
+      return false; 
+};
+
+function comprobarIntento() {
+
+}
