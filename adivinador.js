f920c99f8b7848098dd71357db9e9f2e74b6a225
emiezeoc <emiezeoc@gmail.com>
2023-06-25 16:33:27 -0300

    Update adivinador.js

--- adivinador.js
+++ adivinador.js
@@ -2,11 +2,11 @@ let numeroAleatorio = Math.floor(Math.random() * 100) + 1;
 const $botonEnviar = document.querySelector("#enviar");
 let numeroDeIntentos = 0;
 const $valorIntentoUsuario = document.querySelector("#input-intento");
-const arrayIntentos = [];
-const $intentosAnteriores = document.querySelector("#intentos")
+let arrayIntentos = [];
+let $intentosAnteriores = document.querySelector("#intentos")
 const $altoObajo = document.querySelector("#alto-o-bajo")
 const $resultado = document.querySelector("#resultado")
-
+const $botonResetear = document.querySelector("#resetear")
 
 
 $botonEnviar.onclick = function () {
@@ -20,20 +20,34 @@ $botonEnviar.onclick = function () {
   return false;
 };
 
+$botonResetear.onclick = function() {
+  numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+  numeroDeIntentos= 0;
+  arrayIntentos = []
+  $altoObajo.textContent = ""
+  $resultado.textContent = ""
+  $intentosAnteriores.textContent = ""
+  ocultarBotonResetear()
+  mostrarBotonEnviar()
+
+
+  return false;
+}
+
 function comprobarIntento() {
   let intentoDeUsuario = Number($valorIntentoUsuario.value);
 
   if (intentoDeUsuario == numeroAleatorio) {
     $resultado.textContent = "Felicidades, adivinaste el numero"
-    return false;
-  }
-  
-  if (numeroDeIntentos > 10 ) {
-    $resultado.textContent = "Se te agotaron los intentos"
+    ocultarBotonEnviar();
+    mostrarBotonResetear()
     return false;
   }
 
-  
+  if (numeroDeIntentos >= 10 ) {
+    $resultado.textContent = "Se te agotaron los intentos"
+    return false;
+  }
 
   if (intentoDeUsuario < numeroAleatorio) {
     arrayIntentos.push(intentoDeUsuario);
@@ -42,6 +56,7 @@ function comprobarIntento() {
 
     return false;
   }
+
   if (intentoDeUsuario > numeroAleatorio) {
     arrayIntentos.push(intentoDeUsuario);
     $intentosAnteriores.textContent = $intentosAnteriores.textContent + "  " + intentoDeUsuario
@@ -52,18 +67,24 @@ function comprobarIntento() {
 
 }
 
-function finalizarJuego() {
-      ocultarBotonEnviar()
-      mostrarBotonResetear()
+function mostrarBotonEnviar() {
+  $botonEnviar.className = ""
 
-  
+  return false;
 }
 
 function ocultarBotonEnviar() {
+  $botonEnviar.className = "oculto"
   return false;
 }
 
 function mostrarBotonResetear(){
+  $botonResetear.className = ""
+  return false;
+}
+
+function ocultarBotonResetear() {
+  $botonResetear.className = "oculto"
   return false;
 }
 
